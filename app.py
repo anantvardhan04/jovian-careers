7f25111eecfafeb24cb8795eb79311933e96bc2e
anantvardhan04 <66135657+anantvardhan04@users.noreply.github.com>
2024-06-27 11:57:08 +0000

    Added dynamic values

--- app.py
+++ app.py
@@ -3,10 +3,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    "id": 1,
+    "title": "DevOps Engineer",
+    "salary": "Rs 60,00,000",
+    "location": "Jaipur, India"
+}, {
+    "id": 2,
+    "title": "Backend Engineer",
+    "location": "Bengaluru, India"
+}, {
+    "id": 3,
+    "title": "Business Analyst",
+    "salary": "$150,000",
+    "location": "San Francisco, USA"
+}, {
+    "id": 4,
+    "title": "Project Manager",
+    "salary": "Rs 80,00,000",
+    "location": "Mumbai, India"
+}]
+
 
 @app.route('/')
 def helloworld():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
 
 
 if (__name__ == "__main__"):
